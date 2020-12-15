19fbc8f799836b716f1976aa4061683a3e15ef4e
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2020-12-14 22:34:34 -0800

    can send a messages and other user can view that messages

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,27 @@
 // Make connection
 var socket = io.connect('http://localhost:4000');
+
+// Query DOM
+var message = document.getElementById('message'),
+      user = document.getElementById('user'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
+
+// Emit events
+btn.addEventListener('click', function(){
+  socket.emit('chat', {
+      message: message.value,
+      user: user.value
+  });
+  message.value = "";
+});
+
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
+})
+
+// Listen for events
+socket.on('chat', function(data){
+    output.innerHTML += '<p><strong>' + data.user + ': </strong>' + data.message + '</p>';
+});
