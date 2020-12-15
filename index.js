dfec0459eb35c90df38d7dfae8c8158439af01ec
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2020-12-14 22:39:52 -0800

    user can see when other users is typing

--- index.js
+++ index.js
@@ -16,10 +16,14 @@ io.on('connection', (socket) => {
 
     console.log('made socket connection', socket.id);
 
-    // Handle chat event
+    // User chat event
     socket.on('chat', function(data){
         // console.log(data);
         io.sockets.emit('chat', data);
+    });
+    // User typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
     });
 
 });
--- public/chat.js
+++ public/chat.js
@@ -18,10 +18,14 @@ btn.addEventListener('click', function(){
 });
 
 message.addEventListener('keypress', function(){
-    socket.emit('typing', handle.value);
+    socket.emit('typing', user.value);
 })
 
 // Listen for events
 socket.on('chat', function(data){
     output.innerHTML += '<p><strong>' + data.user + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 });
